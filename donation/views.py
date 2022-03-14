69a26b7bf61307997a0090d7ebc0a5974ba15e6c
BuildTools <unconfigured@null.spigotmc.org>
2022-03-15 00:18:30 +0200

    Change admin and added donation

--- donation/views.py
+++ donation/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-from volunteers.models import Volunteer
+from .forms import DonationForm
+from .models import Donation
 # Create your views here.
 def index(request):
-   
-    return render(request,'donation/index.html')
+    form = DonationForm()
+    donations = Donation.objects.all()
+    if request.method == 'POST':
+        form = DonationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {
+        'form': form,
+        'donations': donations
+    }
+        
+    return render(request,'donation/index.html',context)
